a2bf417d606ea2789a71f03c6c86fa7bbbbb7409
Posiislight <adelekeolamiposi@gmail.com>
2025-09-01 15:37:08 +0100

    added pinging call to the backend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,19 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-
+import { useEffect } from "react";
 const Home = () => {
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/api/v1/home/`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
   return (
     <div className="w-full min-h-screen bg-gray-100 dark:bg-black">
       <Navbar />
